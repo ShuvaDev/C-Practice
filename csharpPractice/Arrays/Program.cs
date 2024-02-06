2af3ce8f5d8624dc20eda9547b574bb549d0f927
Shuva Dev <s.r.shuvadeb@gmail.com>
2024-02-06 23:29:47 +0600

    Classes added

--- csharpPractice/Arrays/Program.cs
+++ csharpPractice/Arrays/Program.cs
@@ -1,37 +1,37 @@
-﻿//// single dimensional array
-//int[] a1 = [10, 20, 30];
-//int[] a2 = {10, 20, 30};
-//int[] a3 = new int[3];
-//a3[0] = 10; a3[1] = 20; a3[2] = 20;
-//int[] a4 = new int[] {10, 20, 30};
+﻿// single dimensional array
+int[] a1 = [10, 20, 30];
+int[] a2 = { 10, 20, 30 };
+int[] a3 = new int[3];
+a3[0] = 10; a3[1] = 20; a3[2] = 20;
+int[] a4 = new int[] { 10, 20, 30 };
 
-//Console.WriteLine(a4.Length); // 3
-//Console.WriteLine(a4.GetLength(0)); // 3
+Console.WriteLine(a4.Length); // 3
+Console.WriteLine(a4.GetLength(0)); // 3
 
-//// multi dimensional array
-//int[,] a5 = new int[,] { { 10, 20 }, { 30, 40 } };
-//int[,] a6 = new int[2, 2] { { 10, 20 }, { 30, 40 } };
-//a6[1, 1] = 100;
+// multi dimensional array
+int[,] a5 = new int[,] { { 10, 20 }, { 30, 40 } };
+int[,] a6 = new int[2, 2] { { 10, 20 }, { 30, 40 } };
+a6[1, 1] = 100;
 
-//Console.WriteLine(a5.Length); // 4
-//Console.WriteLine(a5.GetLength(0)); // 2
+Console.WriteLine(a5.Length); // 4
+Console.WriteLine(a5.GetLength(0)); // 2
 
-//// 2D jagged array (arrays of array)
-//int[][] jarr1 = new int[][]
-//{
-//    new int[] {10, 20, 30 },
-//    new int[] {40, 50}
-//};
+// 2D jagged array (arrays of array)
+int[][] jarr1 = new int[][]
+{
+    new int[] {10, 20, 30 },
+    new int[] {40, 50}
+};
 
-//int[][] jarr2 = new int[2][];
-//jarr2[0] = new int[] { 10, 20, 30 };
-//jarr2[1] = new int[] { 40, 50 };
+int[][] jarr2 = new int[2][];
+jarr2[0] = new int[] { 10, 20, 30 };
+jarr2[1] = new int[] { 40, 50 };
 
-//int[][] jarr3 = new int[][]
-//{
-//    [10, 20, 30],
-//    [40, 50]
-//};
+int[][] jarr3 = new int[][]
+{
+    [10, 20, 30],
+    [40, 50]
+};
 
 // 3D jagged array (arrays of jagged array)
 int[][][] jarr4 = new int[][][]
---
+++ csharpPractice/Class/WatterBottle.cs
@@ -0,0 +1,41 @@
+﻿namespace Classes;
+
+public class WaterBottle
+{
+    private readonly double _capacity;
+    private double _water;
+
+    public string Model { get; private set; }
+
+    public double Water
+    {
+        get
+        {
+            return _water;
+        }
+        set
+        {
+            if (value >= 0)
+            {
+                _water = value;
+            }
+        }
+    }
+
+    public WaterBottle(double capacity)
+    {
+        _capacity = capacity;
+        Model = "ABC";
+    }
+
+    public WaterBottle(double capacity, double water) : this(capacity)
+    {
+        _water = water;
+    }
+
+    public void AddWater(double amount)
+    {
+        if (_water + amount <= _capacity)
+            _water += amount;
+    }
+}
